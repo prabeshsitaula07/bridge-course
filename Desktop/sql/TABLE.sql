a1734cedc146d3559f9907fce169d6acb659bcbf
prabeshsitaula07 <sitaulaprabesh07@gmail.com>
2023-12-06 22:39:20 +0545

    first commit

---
+++ Desktop/sql/TABLE.sql
@@ -0,0 +1,12 @@
+-- STUDENT ID NAME    MAJOR
+--     1      JACK     BIO
+--     2      KATE     SOCIAL
+--     3      CHARLIE  ENGLISH
+--     4      JACK     BIO
+--     5      MIKE     COMP SCI
+
+CREATE TABLE student(
+    student_id INT PRIMARY KEY,
+    name VARCHAR(15),
+    major VARCHAR(15)
+);
